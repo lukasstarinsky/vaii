39a9edf49d944a8a1ebb711b4536ad6df23b27b3
lukasstarinsky <lukas.starinsky8@gmail.com>
2023-11-13 18:33:10 +0100

    Add db connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+// Config
+dotenv.config();
 const app = express();
+const port = process.env.PORT || 8080;
 
+// Routes
 import * as Routes from "./routes";
 app.use("/auth", Routes.Auth);
 //app.use("/forum", Routes.Forum);
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Server running at port ${port}`);
+
+    try {
+        await mongoose.connect(`${process.env.MONGO_DB_URL}`);
+        console.log("Database connected");
+    } catch (err: any) {
+        console.error("Failed to connect to database.");
+        console.error(err);
+        process.exit(1);
+    }
 });

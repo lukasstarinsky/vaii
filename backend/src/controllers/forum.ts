4a561c706b17cae94633e062d101baf0dc46dfbe
lukasstarinsky <lukas.starinsky8@gmail.com>
2023-12-07 00:45:53 +0100

    Add backend thread update, delete

--- backend/src/controllers/forum.ts
+++ backend/src/controllers/forum.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { Thread, ThreadDocument } from "../models/Thread";
 import { body, validationResult } from "express-validator";
 import { UserDocument } from "../models/User";
+import mongoose from "mongoose";
 
 const validCategories = ["news", "general", "media"];
 
@@ -43,6 +44,9 @@ export async function GetThreads(req: Request, res: Response, next: NextFunction
 
 export async function GetThread(req: Request, res: Response, next: NextFunction) {
     try {
+        if (!mongoose.isValidObjectId(req.params.threadId))
+            return res.status(404).send("Thread not found.");
+
         const thread = await Thread.findById(req.params.threadId).populate("author", "username");
 
         if (!thread)
@@ -85,6 +89,48 @@ export async function CreateThread(req: Request, res: Response, next: NextFuncti
 
         res.status(201).send(newThread.id);
      } catch (err: any) {
+        next(err);
+    }
+}
+
+export async function DeleteThread(req: Request, res: Response, next: NextFunction) {
+    try {
+        if (!mongoose.isValidObjectId(req.params.threadId))
+            return res.status(404).send("Thread not found.");
+
+        const deleted = await Thread.findByIdAndDelete(req.params.threadId);
+        if (deleted)
+            res.status(200).send("Thread deleted.");
+        else
+            res.status(404).send("Thread not found.");
+    } catch (err: any) {
+        next(err);
+    }
+}
+
+export async function UpdateThread(req: Request, res: Response, next: NextFunction) {
+    try {
+        if (!mongoose.isValidObjectId(req.params.threadId))
+            return res.status(404).send("Thread not found.");
+
+        const thread = await Thread.findById(req.params.threadId);
+        if (!thread)
+            return res.status(404).send("Thread not found.");
+        /* else if // Todo check admin rights/author */
+
+        let update: any = {};
+        if (req.body.title)
+            update["title"] = req.body.title;
+        if (req.body.description)
+            update["description"] = req.body.description;
+        if (req.body.category) {
+            if (validCategories.includes(req.body.category))
+                update["category"] = req.body.category;
+        }
+
+        await thread.updateOne({ $set: update });
+        return res.status(200).send("Thread updated.");
+    } catch (err: any) {
         next(err);
     }
 }
--- backend/src/routes/forum.ts
+++ backend/src/routes/forum.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import * as Forum from "../controllers/forum";
+import * as Check from "../middleware/permissions";
 
 const router = express.Router();
 
 router.get("/", Forum.Summary);
 
 router.route("/thread/:threadId")
+      .all(Check.IsLoggedIn)
       .get(Forum.GetThread)
+      .delete(Forum.DeleteThread)
+      .patch(Forum.UpdateThread)
       .post(/*Forum.CreatePost*/);
 
 router.route("/category/:category")
+      .all(Check.IsLoggedIn)
       .get(Forum.GetThreads)
       .post(Forum.CreateThread);
 
